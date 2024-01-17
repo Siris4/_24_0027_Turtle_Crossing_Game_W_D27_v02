121034ab8f83ae2d9bfcc905ec9f97950b4c058e
Siris <97780529+Siris4@users.noreply.github.com>
2024-01-17 13:52:34 -0600

    Add files via upload

---
+++ __2x__Turtle_Crossing_ScoreboardModule_v02_W__231101.py
@@ -0,0 +1,25 @@
+
+from turtle import Turtle
+
+#handles the scoreboard and the Game Over sequence.
+
+FONT = ("Courier", 24, "normal")
+
+# TODO 5: Create a Scoreboard and the Game Over sequence:
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.hideturtle()
+        self.penup()
+        self.color("black")
+        # self.score = 0
+        self.level = 1
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.goto(-250, 250)
+        self.write(f"Level: {self.level}", align="left", font=FONT)
+
+
